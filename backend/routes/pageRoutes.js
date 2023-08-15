1f3aca0d85322e4668690542c58a2493e90a1b4c
Daniel Peretz <danielpertz22@gmail.com>
2023-08-15 17:53:32 +0300

    .

--- backend/routes/pageRoutes.js
+++ backend/routes/pageRoutes.js
@@ -51,19 +51,15 @@ router.get("/vendors", (req, res) => {
 router.get("/maintain_users", (req, res) => {
     res.sendFile(views_dir + "maintain_users.html");
 });
-router.get("/products", (req, res) => {
-    res.sendFile(views_dir + "products.html");
-});
+
+
 router.get("/cart", (req, res) => {
     res.sendFile(views_dir + "cart.html");
 });
 
+
 router.get("/products", (req, res) => {
     res.sendFile(views_dir + "products.html");
-});
-
-router.get("/cart", (req, res) => {
-    res.sendFile(views_dir + "cart.html");
 });
 
 module.exports = router;
