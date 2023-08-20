7a5044a8bb755b58f9b8e8bf2fd42d864095f67b
Daniel Nagornov <danieln056@gmail.com>
2023-08-20 18:52:35 +0300

    fix image upload in product edit page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,59 +1,60 @@
-const express = require("express");
-const db_product = require("../db/db_product");
-const {ObjectId} = require("mongodb");
-const utils = require("../util");
-const util = require("../util");
+const express = require('express');
+const db_product = require('../db/db_product');
+const { ObjectId } = require('mongodb');
+const utils = require('../util');
+const util = require('../util');
 
 const router = express.Router();
 
-router.post("/create_product", (req, res) => {
+router.post('/create_product', (req, res) => {
     session = req.session;
 
-    const {
-        name,
-        description,
-        price,
-        stock,
-        category,
-        vendor,
-        platform
-    } = req.body;
+    const { name, description, price, stock, category, vendor, platform } = req.body;
+
+    console.log('create product: ', req.files);
 
     // Save image
-    const {image} = req.files;
+    const { image } = req.files;
     // TODO:Add more checks for the uploaded image
     if (!image) {
-        return res.status(400).send("image upload error");
+        return res.status(400).send('image upload error');
     }
-    image.mv(__dirname + "/../upload/" + image.name);
+    image.mv(__dirname + '/../upload/' + image.name);
 
     // Save product to DB
-    db_product.addProduct(name, description, category, platform, vendor, parseInt(stock), image.name, parseFloat(price), session.user._id).then((addedProduct) => {
-        res.status(200).send("product added");
-    }).catch((err) => {
-        res.status(400).send("Couldn't add product");
-    });
+    db_product
+        .addProduct(name, description, category, platform, vendor, parseInt(stock), image.name, parseFloat(price), session.user._id)
+        .then((addedProduct) => {
+            res.status(200).send('product added');
+        })
+        .catch((err) => {
+            res.status(400).send("Couldn't add product");
+        });
 });
 
-router.get("/get_products", (req, res) => {
+router.get('/get_products', (req, res) => {
     db_product.getProducts().then((q) => {
         res.send(q);
     });
 });
 
-router.put("/update_product", (req, res) => {
-    console.log("\n----------- update product -----------\n");
-    console.log("body: \n", req.body);
-    console.log("img: \n", req.files);
+router.put('/update_product', (req, res) => {
+    console.log('\n----------- update product -----------\n');
+    console.log('body: \n', req.body);
+    console.log('img: \n', req.files);
+
+    var image;
 
     if (req.files) {
-        const {image} = req.files;
-        image.mv(__dirname + "/../upload/" + image.name);
+        const { editImage } = req.files;
+        image = editImage;
+        console.log('help meeee', image);
+        image.mv(__dirname + '/../upload/' + image.name);
     }
 
     db_product.getProduct(req.body.productId).then((new_prod) => {
-        new_prod.name = req.body.name ? req.body.name : "";
-        new_prod.description = req.body.description ? req.body.description : "";
+        new_prod.name = req.body.name ? req.body.name : '';
+        new_prod.description = req.body.description ? req.body.description : '';
         new_prod.category_id = new ObjectId(req.body.category);
         new_prod.platform_id = new ObjectId(req.body.platform);
         new_prod.vendor_id = new ObjectId(req.body.vendor);
@@ -66,20 +67,26 @@ router.put("/update_product", (req, res) => {
                 changed_by: new ObjectId(req.session.user._id)
             });
         }
-        new_prod.save().then(() => {
-            res.status(200).send();
-        }).catch((err) => {
-            res.status(400).send(err);
-        });
+        new_prod
+            .save()
+            .then(() => {
+                res.status(200).send();
+            })
+            .catch((err) => {
+                res.status(400).send(err);
+            });
     });
 });
 
-router.delete("/delete_product", (req, res) => {
-    db_product.findAndDeleteById(req.body.productId).then(() => {
-        res.status(200).send();
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
+router.delete('/delete_product', (req, res) => {
+    db_product
+        .findAndDeleteById(req.body.productId)
+        .then(() => {
+            res.status(200).send();
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
 });
 
 module.exports = router;
