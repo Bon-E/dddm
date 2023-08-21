5b2e6f55f07d1307ca2aac45c0b3a65dba6b665b
Daniel Peretz <danielpertz22@gmail.com>
2023-08-21 18:28:37 +0300

    החיפוש עובד

--- frontend/controller/products.js
+++ frontend/controller/products.js
@@ -92,20 +92,20 @@ function populateProductCards(products) {
   const imagesContainer = $('#images');
 
   products.forEach(product => {
-    const card = `
-      <div class="col">
-        <div class="card">
-          <img src="${product.image}" class="card-img-top" alt="${product.name}">
-          <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text">${findMyPrice(product)} USD</p>
-            <button type="button" class="btn btn-primary add-to-cart-btn" id="${(product._id)}">Add to cart</button>
+      const card = `
+          <div class="col">
+              <div class="card">
+                  <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                  <div class="card-body">
+                      <h5 class="card-title">${product.name}</h5>
+                      <p class="card-text">${product.description}</p>
+                      <p class="card-text">${findMyPrice(product)} USD</p>
+                      <button type="button" class="btn btn-primary add-to-cart-btn" id="${product._id}">Add to cart</button>
+                  </div>
+              </div>
           </div>
-        </div>
-      </div>
-    `;
-    imagesContainer.append(card);
+      `;
+      imagesContainer.append(card);
   });
 }
 function addToCart(product) {
